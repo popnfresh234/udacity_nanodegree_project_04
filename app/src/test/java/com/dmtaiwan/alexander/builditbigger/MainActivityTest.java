e918c3e36ef7c47682ee13c99eb67734b094b9c0
Alex <ajhollid@gmail.com>
2015-07-16 16:06:39 +0800

    Working endpoint test

--- app/src/test/java/com/dmtaiwan/alexander/builditbigger/MainActivityTest.java
+++ app/src/test/java/com/dmtaiwan/alexander/builditbigger/MainActivityTest.java
@@ -3,19 +3,14 @@ package com.dmtaiwan.alexander.builditbigger;
 import junit.framework.TestCase;
 
 /**
- * Created by Alexander on 7/12/2015.
+ * Created by Alexander on 7/16/2015.
  */
 public class MainActivityTest extends TestCase {
 
-    public void testTestString() throws Exception {
-        MainActivity mainActivity = new MainActivity();
-        assert mainActivity.resultString()== null;
-    }
 
     public void testGetJoke() throws Exception {
         MainActivity mainActivity = new MainActivity();
         String joke = mainActivity.getJoke();
         assertNotNull(joke);
-
     }
 }
